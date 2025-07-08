2031560feec6aaa866184f6512deaca782daeeef
Joonas <joonas.aapro@mainiotech.fi>
2025-07-08 16:23:05 +0300

    Update progressbar

--- decidim-budgets_booth/app/packs/src/decidim/budgets_booth/progressFixed.js
+++ decidim-budgets_booth/app/packs/src/decidim/budgets_booth/progressFixed.js
@@ -2,6 +2,10 @@ $(() => {
   const progressElement = document.querySelector(".vote-progress-container");
   const spacer = document.querySelector(".progress-spacer");
   const summary = document.querySelector(".budget-summary__total");
+  const selectedProjects = document.getElementById("order-selected-projects");
+  const budgetProgress = document.querySelector(".budget-progress");
+  const progressMeter = document.querySelector(".progress-meter");
+  const progressReady = document.querySelector(".progress-ready-button");
   let sticky = false;
 
   const checkProgressPosition = () => {
@@ -11,12 +15,24 @@ $(() => {
     if (top <= 0 && !sticky) {
       const height = progressElement.offsetHeight;
 
-      progressElement.classList.add("w-full", "fixed", "top-0");
+      progressElement.classList.add("w-full", "fixed", "top-0", "progress-background");
+      selectedProjects.classList.remove("my-4");
+      selectedProjects.classList.add("my-2");
+      budgetProgress.classList.remove("h-12");
+      budgetProgress.classList.add("h-6");
+      progressMeter.classList.add("h-6");
+      progressReady.classList.add("h-6");
       summary.classList.add("hidden");
       spacer.style.height = `${height}px`;
       sticky = true;
     } else if (spacerTop > 0 && sticky) {
-      progressElement.classList.remove("w-full", "fixed", "top-0");
+      progressElement.classList.remove("w-full", "fixed", "top-0", "progress-background");
+      selectedProjects.classList.add("my-4")
+      selectedProjects.classList.remove("my-2");
+      budgetProgress.classList.remove("h-6");
+      budgetProgress.classList.add("h-12");
+      progressMeter.classList.remove("h-6");
+      progressReady.classList.remove("h-6");
       summary.classList.remove("hidden");
       spacer.style.height = "0px";
       sticky = false;
