dc19696321623a3070b25a17353f7809dda67118
Joonas <joonas.aapro@mainiotech.fi>
2026-03-18 09:25:31 +0200

    Delegate selected -method to model in cell instead of new method

--- decidim-budgets_booth/app/cells/concerns/decidim/budgets_booth/project_l_cell_extensions.rb
+++ decidim-budgets_booth/app/cells/concerns/decidim/budgets_booth/project_l_cell_extensions.rb
@@ -7,16 +7,14 @@ module Decidim
       include ::Decidim::BudgetsBooth::VotingSupport
 
       included do
+        delegate :selected?, to: :model
+
         def voting_mode?
           options[:voting_mode]
         end
 
         def show_votes?
           model.component.current_settings.show_votes?
-        end
-
-        def selected?
-          model.selected?
         end
       end
     end
