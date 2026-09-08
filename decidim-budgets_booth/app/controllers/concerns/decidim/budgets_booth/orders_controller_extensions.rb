8ef1fdc7599745582ecdac4c6bbb130232828c4e
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-09-08 09:56:40 +0300

    Fix displaying the checked out order for the current user

--- decidim-budgets_booth/app/controllers/concerns/decidim/budgets_booth/orders_controller_extensions.rb
+++ decidim-budgets_booth/app/controllers/concerns/decidim/budgets_booth/orders_controller_extensions.rb
@@ -52,10 +52,10 @@ module Decidim
         end
 
         def order
-          result = Decidim::Budgets::Order.find_by(decidim_budgets_budget_id: params[:budget_id])
-          return nil unless result && result.checked_out_at.present?
+          return unless current_order
+          return unless current_order.checked_out_at.present?
 
-          result
+          current_order
         end
       end
     end
